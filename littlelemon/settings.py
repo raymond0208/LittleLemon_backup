61f48cd9ed6341269cb9cb6b5ea5b16e49313923
raymond <raymond0208@gmail.com>
2023-10-07 23:37:27 +0800

    add token authentication and djoser user registration

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'restaurant',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +136,12 @@ STATIC_URL = 'restaurant/static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ( 
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
+
+DJOSER={"USER_ID_FIELD":"username"}
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets,generics, permissions
-from rest_framework.decorators import permission_classes
+from rest_framework.decorators import permission_classes,api_view
 from rest_framework.permissions import IsAuthenticated,IsAdminUser
+from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .serializers import UserSerializer,MenuSerializer,BookingSerializer
 from .models import Menu,Booking
@@ -14,11 +15,13 @@ def index(request):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
+
+@permission_classes([IsAuthenticated])    
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
-    
+
+@permission_classes([IsAuthenticated])    
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
@@ -27,3 +30,8 @@ class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     #permission_classes = [permissions.IsAuthenticated]
+    
+@api_view()
+@permission_classes([IsAuthenticated])
+def msg(request):
+    return Response({"message":"This view is protected."})
